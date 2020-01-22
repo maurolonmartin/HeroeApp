f8309229db1dc06c574c09c8633475ba21e529ed
maurolonmartin <maurolonmartin@hotmail.com>
2020-01-21 20:36:44 -0500

    first commit

---
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroe-tarjeta',
+  templateUrl: './heroe-tarjeta.component.html'
+})
+export class HeroeTarjetaComponent implements OnInit {
+
+  @Input() heroe: any = {};
+  @Input() index: number;
+
+  @Output() heroeSelecciondo: EventEmitter<number>;
+
+  constructor(private router: Router) {
+    this.heroeSelecciondo = new EventEmitter();
+   }
+
+  ngOnInit() {
+  }
+
+  verHeroe() {
+    // console.log(this.index);
+    this.router.navigate( ['/heroe', this.index] );
+    // this.heroeSelecciondo.emit( this.index );
+  }
+
+}
